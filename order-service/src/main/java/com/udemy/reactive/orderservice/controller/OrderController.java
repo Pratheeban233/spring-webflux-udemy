b11926c060393ee5bd44a15cebdca6cb210aa010
Pratheeban M <Pratheeban.M@in.ey.com>
2022-11-16 08:30:50 +0530

    Handling failures

--- order-service/src/main/java/com/udemy/reactive/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/udemy/reactive/orderservice/controller/OrderController.java
@@ -4,7 +4,11 @@ import com.udemy.reactive.orderservice.dto.OrderRequestDto;
 import com.udemy.reactive.orderservice.dto.OrderResponseDto;
 import com.udemy.reactive.orderservice.service.OrderFulfillmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -16,8 +20,11 @@ public class OrderController {
     private OrderFulfillmentService service;
 
     @PostMapping
-    public Mono<OrderResponseDto> getOrder(@RequestBody Mono<OrderRequestDto> requestDtoMono) {
-        return this.service.processOrder(requestDtoMono);
+    public Mono<ResponseEntity<OrderResponseDto>> getOrder(@RequestBody Mono<OrderRequestDto> requestDtoMono) {
+        return this.service.processOrder(requestDtoMono)
+                .map(ResponseEntity::ok)
+                .onErrorReturn(WebClientResponseException.class, ResponseEntity.badRequest().build())
+                .onErrorReturn(WebClientRequestException.class, ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build());
     }
 
     @GetMapping("/user/{id}")
